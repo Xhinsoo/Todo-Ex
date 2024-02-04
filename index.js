e6c44713fce5ac2e67cdd194c671f80a2aeabc75
LordNinth <rr621801@gmail.com>
2024-02-04 10:45:04 +0000

    add new task

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const path = require("path");
 
-app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true })); //any form data that comes in from a form or post req, parse it
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs"); //view engine set to ejs
 
-app.get('/home', (req,res)=>{
-    res.render('home')
-})
 
-app.get('/new', (req,res)=>{
-    res.render('new')
-})
-app.listen(3000, (req,res)=>{
-    console.log("listening to port 3000")
-})
+//toDoList array
+const toDoList = [
+  {
+    task: "clean dish",
+  },
+  {
+    task: "clean dish",
+  },
+  {
+    task: "clean dish",
+  },
+];
+
+
+//upon receiving html get req, renders index with toDoList array
+app.get("/index", (req, res) => {
+  res.render("index", { toDoList });
+});
+
+//renders new page
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+//receives post req matching /new path and process it and redirect to prevent double send
+app.post("/new", (req, res) => {
+  const { task } = req.body;
+  toDoList.push({ task });
+  res.redirect("/index");
+});
+
+app.listen(3000, (req, res) => {
+  console.log("listening to port 3000");
+});
